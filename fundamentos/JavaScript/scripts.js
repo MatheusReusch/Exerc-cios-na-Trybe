ac3767096e52becbd24b0695ecb45553297440ab
Matheus Reusch <matheusreusch1@gmail.com>
2021-10-07 20:25:01 -0300

    Java

---
+++ fundamentos/JavaScript/scripts.js
@@ -0,0 +1,12 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = 2 * base + 2 * height;
+let myName = 'Matheus Reusch';
+let birthYear = '2002';
+console.log(area)
+console.log(perimeter)
+console.log(myName)
+console.log(birthYear)
+console.log(base + height)
+
