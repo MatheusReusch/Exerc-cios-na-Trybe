8f3873b490f845bec1e73995882eed62a48257a5
Matheus Reusch <matheusreusch1@gmail.com>
2021-10-08 16:23:54 -0300

    JavaScript

--- fundamentos/JavaScript/scripts.js
+++ fundamentos/JavaScript/scripts.js
@@ -4,9 +4,14 @@ const area = base * height;
 const perimeter = 2 * base + 2 * height;
 let myName = 'Matheus Reusch';
 let birthYear = '2002';
+let trybe = 16;
+if(trybe >= 14 && trybe < 14.40) {console.log("Esquenta");} 
+else if (trybe > 14.40 && trybe < 16.20); {console.log("Course");}
+
+console.log(trybe)
 console.log(area)
 console.log(perimeter)
 console.log(myName)
 console.log(birthYear)
-console.log(base + height)
+console.log(base + height) 
 
---
+++ fundamentos/JavaScript/scripts2.js
@@ -0,0 +1,20 @@
+const nota = 50;
+if(nota >= 80) {console.log("Parabéns, você foi aprovada(o)!")}
+else if (nota >= 60 && nota < 80) {console.log("Você está na nossa lista de espera")}
+else {console.log("Você foi reprovada(o)")}
+const comida = true;
+const bebida = true;
+if (comida == true && bebida == true) {console.log('Obrigado')}
+else {console.log('opa')}
+const currentHour = 5;
+let message = '';
+if (currentHour >= 22) {message = "Não deveríamos comer nada, é hora de dormir"}
+else if (currentHour >= 18 && currentHour < 22) {message ="Rango da noite, vamos jantar :D"}
+else if (currentHour >= 14 && currentHour < 18) {message="Vamos fazer um bolo pro café da tarde?"}
+else if (currentHour > 11 && currentHour < 14) {message="Hora do almoço!!!"}
+else if (currentHour > 4 && currentHour < 11) {message="Hmmm, cheiro de café recém passado"}
+let weekDay = 'quarta';
+if (weekDay == 'segunda' || 'terca' || 'quarta' || 'sexta') {console.log("Oba, mais um dia de aprendizado na Trybe >:D")}
+else {console.log("FINALMENTE, descanso merecido UwU")}
+console.log
+console.log(message) 
