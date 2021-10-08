900f704d26f8c4db0218f1eb49e6d83c7cfa7d19
Matheus Reusch <matheusreusch1@gmail.com>
2021-10-08 18:32:49 -0300

    Programas com JavaScript

---
+++ fundamentos/JavaScript/programs.js
@@ -0,0 +1,39 @@
+console.log('Olá mundo!!')
+
+const a = 2;
+const b = 3;
+const c = 4;
+
+let adicoes = a + b;
+let subtracoes = a - b;
+let multiplicacoes = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+if (a > b) {console.log(a)}
+else if (b > a) {console.log(b)}
+else {console.log('erro')}
+
+if (a > b && a > c) {console.log(a)}
+else if (b > a && b > c) {console.log(b)}
+else if (c > a && c > b) {console.log(c)}
+else {console.log('ERRO')}
+
+if (a > 0) {console.log('Positivo')}
+else if (a < 0) {console.log('Negativo')}
+else {console.log('zero')}
+
+const a1 = 90;
+const a2 = 45;
+const a3 = 45;
+
+if (a1 < 0 || a2 < 0 || a3 < 0) console.log('ERRO')
+else if (a1 + a2 + a3 === 180) {console.log(true)}
+else {console.log(false)}
+
+
+
+
+
+
+
