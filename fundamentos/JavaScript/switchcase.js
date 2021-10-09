0985ab25bfef0f03ca51b4f8a27af5400e184fb9
Matheus Reusch <matheusreusch1@gmail.com>
2021-10-09 15:27:54 -0300

    switch/case

---
+++ fundamentos/JavaScript/switchcase.js
@@ -0,0 +1,18 @@
+let estado = 'reprovado';
+
+switch (estado) {
+    case 'aprovado':
+    console.log('Uhuuul,Parabénss!');
+    break;
+    
+    case 'reprovado':
+    console.log('Sinto muito')
+    break;
+    
+    case 'lista':
+    console.log('voce ainda tem chance')
+    break;
+    
+    default:
+    console.log('invalido')    
+}
