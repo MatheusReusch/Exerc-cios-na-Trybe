0d88fcc20d113273bf36cd51643d7e7886a3df84
Matheus Reusch <matheusreusch1@gmail.com>
2021-12-20 00:33:23 -0300

    Exercicios

--- Front-end/CiclodeVidaeRouter/exercicios12.1/src/App.js
+++ Front-end/CiclodeVidaeRouter/exercicios12.1/src/App.js
@@ -9,13 +9,16 @@ class App extends React.Component {
     this.fetchUser = this.fetchUser.bind(this)
     this.renderUser = this.renderUser.bind(this)
     this.savePerson =  this.savePerson.bind(this)
+    this.generatePerson = this.generatePerson.bind(this)
+    this.desativaBotao = this.desativaBotao.bind(this)
 
     this.state = {
       chracters: [],
-      loading: false,
+      loading: true,
       user: '',
       pessoas: [],
-      storedPessoas: []
+      storedPessoas: [],
+      disableButton: false
     }
   }
 
@@ -28,23 +31,57 @@ class App extends React.Component {
   }
 
   fetchUser() {
-      fetch('https://api.randomuser.me/')
+    this.setState(
+      {loading: true},
+      async () => {
+        fetch('https://api.randomuser.me/')
       .then((response) => response.json())
-      .then((data) => 
-      this.setState((prevState, _props) => ({pessoas: [...prevState.pessoas, data.results]}))
+      .then((data) => {
+        this.setState((prevState, _props) => ({loading:false, pessoas: [...prevState.pessoas, data.results]}))
+      }
       )
+      }
+    )  
   }
 
   renderUser() {
     return this.state.pessoas.map((pessoa) => {
       return <>
       <p key={pessoa[0].last}>{pessoa[0].email + ' ' + pessoa[0].name.first + ' ' + pessoa[0].name.last + ' ' + pessoa[0].dob.age}</p>
-      <img src={pessoa[0].picture.thumbnail}></img>
+      <img alt='foto' src={pessoa[0].picture.thumbnail}></img>
       </>
     })
   }
 
   savePerson() {
+    this.setState(
+      this.fetchUser(),
+      () => ({loading: false})
+    )
+  }
+
+  desativaBotao() {
+    this.setState({disableButton: true})
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    let proxima = nextState.pessoas[nextState.pessoas.length - 1]
+    if (proxima !== undefined) {
+     if (proxima[0].dob.age > 60) { 
+      document.getElementById('btn').disabled = 'true'
+      return false
+     }
+     if (proxima[0].dob.age <= 50) { 
+      document.getElementById('btn').disabled = false
+      console.log(document.getElementById('btn').disabled)
+      return true
+     }
+    }
+    return true
+  }
+
+  generatePerson() {
+    this.state.pessoas.pop()
     this.fetchUser()
   }
 
@@ -54,13 +91,13 @@ class App extends React.Component {
   }
   
   render () {
-    console.log(this.state.chracters)
-    console.log(this.state.pessoas)
+    const loadingElement = <p>Loading...</p>
     return (
          <>
          <section id='pessoas'>
-         <>{this.state.pessoas[0] ? this.renderUser() : 'Loading...'}</>
-         <button onClick={this.savePerson}>Salvar pessoa</button>
+         <>{this.state.loading ? loadingElement : this.renderUser()}</>
+         <button id='btn' onClick={this.savePerson}>Salvar pessoa</button>
+         <button onClick={this.generatePerson}>Gerar outra pessoa</button>
          </section>
          <div>{this.state.chracters.map((pessoa) => {
            return <div key={pessoa.id}><p >{pessoa.name}</p><img alt='imagem' src={pessoa.image}></img></div>
